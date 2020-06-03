1c504d2927882d1a2296a100f3fa5fa5a91bdb46
Muhibullah-09 <mc2840696@gmail.com>
2020-06-03 06:24:39 +0500

    Update name
    Update some change

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          Updated By Muhibullah Khan Kamali
         </p>
         <a
           className="App-link"
